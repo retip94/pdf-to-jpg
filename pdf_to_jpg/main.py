b3b2e44c372bb36bef3757d428c61d3cd0a6c474
retip94 <38405004+retip94@users.noreply.github.com>
2021-03-10 08:07:13 +0100

    fixed pages bug

--- pdf_to_jpg/main.py
+++ pdf_to_jpg/main.py
@@ -6,19 +6,19 @@ import locale
 def main():
     pdfs = get_all_pdfs()
     for pdf in pdfs:
-        name = pdf.split('.p')[0]
+        name = pdf.split('.pdf')[0]
         pdf_to_jpg(
             pdf,
-            f'{name}.jpeg'
+            name
         )
 
 
-def pdf_to_jpg(pdf_input_path, jpeg_output_path):
+def pdf_to_jpg(pdf_input_path, jpeg_name):
     args = ["pdf2jpeg",  # actual value doesn't matter
             "-dNOPAUSE",
             "-sDEVICE=jpeg",
             "-r300",
-            "-sOutputFile=" + jpeg_output_path,
+            f'-sOutputFile={jpeg_name}-%03d.jpg',
             pdf_input_path]
     encoding = locale.getpreferredencoding()
     args = [a.encode(encoding) for a in args]
